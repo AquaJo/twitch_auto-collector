84ad90f08a5adb3653e68357ab41213fe17279e6
AquaJo <84229101+AquaJo@users.noreply.github.com>
2022-08-21 23:44:39 +0200

    new project structure and logic

--- content.js
+++ content.js
@@ -1,16 +1,20 @@
 window.onload = () => {
-    let mutationObserver = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
+    let mutationObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
             if (mutation.target && mutation.target.classList && document.getElementsByClassName('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 ffyxRu gjXDMG')[0]) {
                 let btn = document.getElementsByClassName('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 ffyxRu gjXDMG')[0];
-                setTimeout(function() {
-                    btn.click();
-                },getRandomInt(3000,6000))
+                setTimeout(function () {
+                    try {
+                        btn.click();
+                    } catch (e) {
+
+                    }
+                }, getRandomInt(3000, 6000))
                 console.log("clicked bonus --> from twitch - auto-collector");
             }
         });
     });
-    mutationObserver.observe(document.body, {attributes: true, subtree: true, childList : true, characterData: true});
+    mutationObserver.observe(document.body, { attributes: true, subtree: true, childList: true, characterData: true });
 }
 
 
