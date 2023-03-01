3cab4d5a43d01c4bef939f0323ef1668c37cdbbe
AquaJo <84229101+AquaJo@users.noreply.github.com>
2023-03-01 18:56:04 +0100

    tidy code and  tampermonkey.js updated

--- tampermonkey.js
+++ tampermonkey.js
@@ -11,28 +11,30 @@
 
 // working on tampermonkey and violentmonkey
 
-(function() {
+(function () {
     'use strict';
 
     window.onload = () => {
         let mutationObserver = new MutationObserver(function (mutations) {
             mutations.forEach(function (mutation) {
-                if (mutation.target && mutation.target.classList && document.getElementsByClassName('ScCoreButton-sc-ocjdkq-0 ScCoreButtonSuccess-sc-ocjdkq-5 ibtYyW iVIehm')[0]) {
-                    let btn = document.getElementsByClassName('ScCoreButton-sc-ocjdkq-0 ScCoreButtonSuccess-sc-ocjdkq-5 ibtYyW iVIehm')[0];
-                    setTimeout(function () {
-                        try {
-                            btn.click();
-                        } catch (e) {
-    
-                        }
-                    }, getRandomInt(3000, 6000));
+                if (mutation.target && mutation.target.classList && document.querySelector('[class*="claimable-bonus__icon"]')) {
+                    let btns = document.querySelectorAll('[class*="claimable-bonus__icon"]');
+                    btns.forEach(function (btn) {
+                        setTimeout(function () {
+                            try {
+                                btn.click();
+                            } catch (e) {
+
+                            }
+                        }, getRandomInt(3000, 6000));
+                    });
                 }
             });
         });
         mutationObserver.observe(document.body, { attributes: true, subtree: true, childList: true, characterData: true });
     }
-    
-    
+
+
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
